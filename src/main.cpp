0c2e9e0835075a57a18ffe27a71aa06c169ef6a3
Ryan Chen <ryanachen@gmail.com>
2024-12-21 23:06:26 -0800

    Tested use of binary semaphores to manage task execution

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,59 @@
 #include <Arduino.h>
 
+TaskHandle_t emailTaskHandle = NULL;
+
+/*
+Takes hourly/daily readings from a capacitive soil moisture sensor.
+Notifies email task (and watering task if below moisture threshold).
+*/
+void readSensorData(void * parameters){
+  for(;;){
+
+    Serial.println("Reading Soil Moisture Data...");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    Serial.println("Notify Email Task");
+    xTaskNotifyGive(emailTaskHandle);
+
+  }
+}
+
+/*
+Send email with information about soil moisture.
+*/
+void sendStatusEmail(void * parameters){
+  for(;;){
+
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY); //block indefinitely until notified
+    Serial.println("Hello from Email Task");
+    
+  }
+}
+
 void setup() {
 
   Serial.begin(115200);
 
-}
+  xTaskCreate(
+    readSensorData,
+    "SensorTask",
+    1000,
+    NULL,
+    1,
+    NULL
+  );
+
+  xTaskCreate(
+    sendStatusEmail,
+    "EmailTask",
+    1000,
+    NULL,
+    2,
+    &emailTaskHandle
+  );
+
+} 
 
 void loop() {
-
   // The loop function can be empty when using FreeRTOS
-  Serial.println("Hello World!");
-  delay(1000);
-  
 }
